1055f932ddab4465e555a0b44560e37879525140
chattyadmin <chattyadmin>
2005-02-12 11:16:51 +0000

    Zeitanzeige im ServerLog

--- Chatty/chatty/gui/ServerWindow.java
+++ Chatty/chatty/gui/ServerWindow.java
@@ -1,23 +1,36 @@
 package chatty.gui;
 
+import java.util.*;
 /**
  * Konsolenfenster für Serverapplikation
  */
 
 public class ServerWindow implements ChatInstance {
 
-	public void updateNetStatus() {
+	private String getTime(){
+	    GregorianCalendar cal = new GregorianCalendar();
+	    String[] time = new String[3];
+	    time[0] = cal.get(Calendar.HOUR_OF_DAY) + ":"; 
+	    time[1] = cal.get(Calendar.MINUTE) + ":";
+	    time[2] = cal.get(Calendar.SECOND)+" ";     
+	    for(int i=0;i<3;i++)
+	        if(time[i].length()==1)
+	            time[i]="0"+time[i];
+	    return time[0]+time[1]+time[2];
+	}
+    
+    public void updateNetStatus() {
 	}
 
 	public void appendText(String text) {
-		System.out.println(text);
+		System.out.println(getTime()+text);
 	}
 
 	public void appendError(String text) {
-		System.err.println(text);
+		System.err.println(getTime()+text);
 	}
 
-	public void clearText() {
+	public void clearText() {	    
 	}
 
 }
