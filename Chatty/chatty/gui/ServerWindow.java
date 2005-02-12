365768cab4a5d05358490159c68e324fabc7cb93
chattyadmin <chattyadmin>
2005-02-12 11:22:58 +0000

    ZeitBug behoben

--- Chatty/chatty/gui/ServerWindow.java
+++ Chatty/chatty/gui/ServerWindow.java
@@ -10,13 +10,13 @@ public class ServerWindow implements ChatInstance {
 	private String getTime(){
 	    GregorianCalendar cal = new GregorianCalendar();
 	    String[] time = new String[3];
-	    time[0] = cal.get(Calendar.HOUR_OF_DAY) + ":"; 
-	    time[1] = cal.get(Calendar.MINUTE) + ":";
-	    time[2] = cal.get(Calendar.SECOND)+" ";     
+	    time[0] = cal.get(Calendar.HOUR_OF_DAY)+""; 
+	    time[1] = cal.get(Calendar.MINUTE)+"";
+	    time[2] = cal.get(Calendar.SECOND)+"";     
 	    for(int i=0;i<3;i++)
 	        if(time[i].length()==1)
 	            time[i]="0"+time[i];
-	    return time[0]+time[1]+time[2];
+	    return time[0]+":"+time[1]+":"+time[2]+" ";
 	}
     
     public void updateNetStatus() {
