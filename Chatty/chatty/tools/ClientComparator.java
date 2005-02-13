a71420ca99e7943702c2e0ceea4d186a969b3d3c
chattyadmin <chattyadmin>
2005-02-13 12:22:27 +0000

    Klienes Makro

--- Chatty/chatty/tools/ClientComparator.java
+++ Chatty/chatty/tools/ClientComparator.java
@@ -6,9 +6,7 @@ public class ClientComparator implements Comparator {
     
     final public int compare(Object o1, Object o2) {
         int ergebnis = o1.toString().compareToIgnoreCase(o2.toString());
-        if (ergebnis == 0)
-            return -1;
-        return ergebnis;
+        return ergebnis==0?-1:ergebnis;
     }
     
 }
